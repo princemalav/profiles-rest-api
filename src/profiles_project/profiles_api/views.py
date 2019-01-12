7acceb0f674f8ac5de2e576f8a4fd82a1c87c449
princemalav <33306779+princemalav@users.noreply.github.com>
2019-01-12 10:53:10 +0530

    New permissions added to the news feed permissions api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -9,7 +9,7 @@ from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class HelloWorldApi(APIView, Response):
@@ -111,7 +111,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
-    permission_classes = (permissions.PostOwnStatus , IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.PostOwnStatus , IsAuthenticated)
 
     def perform_create(self,serializer):
 
